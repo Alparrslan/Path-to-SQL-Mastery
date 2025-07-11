ca55f54aed80f163848bb170925e9369145cbab6
Ali Alpaslan Akgüzel <alpaslan1985@hotmail.com>
2025-07-11 10:49:33 +0300

    Update 18_Subqueries_in_select.sql

--- 18_Subqueries_in_select.sql
+++ 18_Subqueries_in_select.sql
@@ -59,4 +59,3 @@ SELECT id, (SELECT COUNT(*) FROM messages WHERE sender_id = users.id) AS sent_me
 
 -- 20
 SELECT id, (SELECT COUNT(*) FROM uploads WHERE owner_id = users.id) AS uploads FROM users;
-
