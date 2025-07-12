26a5871b32adb8f4875e942d0b3e5bf8d403b52c
Ali Alpaslan Akgüzel <alpaslan1985@hotmail.com>
2025-07-12 10:47:29 +0300

    Update 23_Interval_advanced.sql

--- 23_Interval_advanced.sql
+++ 23_Interval_advanced.sql
@@ -1,4 +1,4 @@
--- INTERVAL Function - Intermediate and Advanced Queries
+-- INTERVAL Function 
 
 -- 1. Users inactive for more than 6 months
 SELECT * FROM users WHERE last_login < CURRENT_DATE - INTERVAL '6 months';
